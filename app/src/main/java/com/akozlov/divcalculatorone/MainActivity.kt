7d72b728578caa818830657c713c546b5d34aeb7
AlexK <a.privet.narnia@gmail.com>
2026-05-13 11:11:32 +0300

    added square unit

--- app/src/main/java/com/akozlov/divcalculatorone/MainActivity.kt
+++ app/src/main/java/com/akozlov/divcalculatorone/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.akozlov.divcalculatorone
 
+import android.R.attr.width
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -20,6 +26,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -87,6 +94,10 @@ fun MainScreen() {
 
         // Передаем текст для отображения
         MyDisplayText(text = resultText)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        SquareUnit()
     }
 }
 
@@ -125,14 +136,32 @@ fun MyDisplayText(text: String) {
     }
 }
 
-
 fun QuotentAsStrind(divider:String, dividend:String): String {
     if (divider.isEmpty() or dividend.isEmpty()) return "0"
     val res = (divider.toDouble()/dividend.toDouble()).toString()
     return res
 }
 
+fun QuotentAsDouble(divider:String, dividend:String): Double {
+    if (divider.isEmpty() or dividend.isEmpty()) return 0.0
+    val res = (divider.toDouble()/dividend.toDouble())
+    return res
+}
 
+@Composable
+fun SquareUnit() {
+    Column(Modifier
+        .size(width=25.dp, height = 25.dp)
+        .border(width = 1.dp, color = Color.Blue),)
+    {
+        Row(modifier = Modifier.fillMaxWidth().height(5.dp)) {
+            Text(text = " " + "." + " ")
+        }
+        Row(modifier = Modifier.fillMaxWidth()) {
 
+            Text(text = "-" + "0" + " ", modifier = Modifier.fillMaxWidth())
+        }
+    }
 
+}
 
