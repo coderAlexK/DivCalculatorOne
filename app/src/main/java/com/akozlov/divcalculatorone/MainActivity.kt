6966dfc2bc87466b5dcddca7f6489296c1339bf2
AlexK <a.privet.narnia@gmail.com>
2026-05-13 13:43:35 +0300

    square unit modified

--- app/src/main/java/com/akozlov/divcalculatorone/MainActivity.kt
+++ app/src/main/java/com/akozlov/divcalculatorone/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.akozlov.divcalculatorone
 
 import android.R.attr.width
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -17,6 +20,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -25,9 +29,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.akozlov.divcalculatorone.ui.theme.DivCalculatorOneTheme
@@ -154,13 +160,33 @@ fun SquareUnit() {
         .size(width=25.dp, height = 25.dp)
         .border(width = 1.dp, color = Color.Blue),)
     {
-        Row(modifier = Modifier.fillMaxWidth().height(5.dp)) {
-            Text(text = " " + "." + " ")
-        }
-        Row(modifier = Modifier.fillMaxWidth()) {
+        Row(modifier = Modifier.padding(top = 3.dp).fillMaxWidth().height(2.dp)
+            ) {
+                Canvas(modifier = Modifier.fillMaxSize()) {
+                   drawCircle(
+                       color = Color.Black,
+                       radius = size.minDimension / 1, // Размер точки
+                       center = center // Центрирование внутри Canvas
 
-            Text(text = "-" + "0" + " ", modifier = Modifier.fillMaxWidth())
+            )
         }
+
+
+        }
+
+
+        Row(modifier = Modifier.fillMaxWidth()
+            ,verticalAlignment = Alignment.CenterVertically) {
+
+            Text(text = "-", textAlign = TextAlign.Center)
+            Text(text = "0", textAlign = TextAlign.Center)
+            Text(text = " ", textAlign = TextAlign.Center)
+        }
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 0.dp),
+            thickness = 2.dp,
+            color = Color.Black
+        )
     }
 
 }
