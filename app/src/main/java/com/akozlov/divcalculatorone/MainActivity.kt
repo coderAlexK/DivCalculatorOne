352c9459d403b8c4819a8a6f91c5854470bad30d
AlexK <a.privet.narnia@gmail.com>
2026-05-13 10:22:00 +0300

    functions renamed

--- app/src/main/java/com/akozlov/divcalculatorone/MainActivity.kt
+++ app/src/main/java/com/akozlov/divcalculatorone/MainActivity.kt
@@ -5,23 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
@@ -48,27 +44,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     // Состояние "поднято" сюда, чтобы все функции имели к нему доступ
-    var inputDivider by remember { mutableStateOf("") }
     var inputDividend by remember { mutableStateOf("") }
+    var inputDivisor by remember { mutableStateOf("") }
     val numericRegex = Regex("[^0-9]")
     var resultText by remember { mutableStateOf("") }
 
     Column(modifier = Modifier.padding(16.dp)) {
         // Передаем значение и способ его изменить
-        DividerField(text = inputDivider,
-            {
-                // Remove non-numeric characters.
-                val stripped = numericRegex.replace(it, "")
-                inputDivider = if (stripped.length >= 10) {
-                    stripped.substring(0..9)
-                } else {
-                    stripped
-                }
-            }
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
         DividendField(text = inputDividend,
             {
                 // Remove non-numeric characters.
@@ -80,9 +62,24 @@ fun MainScreen() {
                 }
             }
         )
+
         Spacer(modifier = Modifier.height(8.dp))
 
-        var res = CalculationUnit(inputDivider, inputDividend)
+        
+        DivisorField(text = inputDivisor,
+            {
+                // Remove non-numeric characters.
+                val stripped = numericRegex.replace(it, "")
+                inputDivisor = if (stripped.length >= 10) {
+                    stripped.substring(0..9)
+                } else {
+                    stripped
+                }
+            }
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        val res = QuotentAsStrind(inputDividend, inputDivisor)
         // Передаем действие при клике
         CalculateButton(onClick = { resultText = res})
 
@@ -93,16 +90,8 @@ fun MainScreen() {
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    DivCalculatorOneTheme {
-
-    }
-}
-
-@Composable
-fun DividerField(text: String, onValueChange: (String) -> Unit) {
+fun DividendField(text: String, onValueChange: (String) -> Unit) {
     TextField(
         value = text,
         onValueChange = onValueChange,
@@ -113,7 +102,7 @@ fun DividerField(text: String, onValueChange: (String) -> Unit) {
 }
 
 @Composable
-fun DividendField(text: String, onValueChange: (String) -> Unit) {
+fun DivisorField(text: String, onValueChange: (String) -> Unit) {
     TextField(
         value = text,
         onValueChange = onValueChange,
@@ -137,9 +126,9 @@ fun MyDisplayText(text: String) {
 }
 
 
-fun CalculationUnit(divider:String, dividend:String): String {
+fun QuotentAsStrind(divider:String, dividend:String): String {
     if (divider.isEmpty() or dividend.isEmpty()) return "0"
-    val res = (divider.toInt()/dividend.toInt()).toString()
+    val res = (divider.toDouble()/dividend.toDouble()).toString()
     return res
 }
 
